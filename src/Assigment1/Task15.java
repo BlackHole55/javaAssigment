2b6d97b682a0543481809afc35647b8a394228a2
unknown <syzdykovmr@gmail.com>
2024-12-21 10:08:56 +0500

    Modify 15 Task

--- src/Assigment1/Task15.java
+++ src/Assigment1/Task15.java
@@ -3,8 +3,12 @@ package Assigment1;
 public class Task15 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print(8);
+            for (int j = 0; j < 10; j++) {
+                if (j == i || j == (10 - (i + 1))) {
+                    System.out.print(8);
+                }else {
+                    System.out.print(' ');
+                }
             }
             System.out.println();
         }
